882bd0f075bed8ff1f78d8f69c76db04e95cf654
mohamed <muhamed.adel96@gmail.com>
2023-01-04 14:01:36 +0200

    first link

---
+++ E-Commerce app/src/test/java/stepDefinitions/registerSD.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+public class registerSD {
+    @Given("User open Browser")
+    public void user_open_Browser (){
+
+    }
+  @And("User navigates to website")
+    public void User_navigates_to_website () {
+
+  }
+
+}
