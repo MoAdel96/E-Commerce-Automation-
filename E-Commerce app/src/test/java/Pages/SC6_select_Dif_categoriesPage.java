97b673a50e98fbb34f3d155a6170a612fd8070df
Adel <muhamed.adel96@gmail.com>
2023-01-08 23:49:59 +0200

    sc6 done with testing

--- E-Commerce app/src/test/java/Pages/SC6_select_Dif_categoriesPage.java
+++ E-Commerce app/src/test/java/Pages/SC6_select_Dif_categoriesPage.java
@@ -1,4 +1,23 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import stepDefinitions.browser;
+
 public class SC6_select_Dif_categoriesPage {
+    public WebElement Category (){
+        By category =By.cssSelector("a[href=\"/apparel\"]");
+        WebElement Category = browser.driver.findElement(category);
+        return Category;
+
+
+    }
+    public WebElement SubCategory (){
+        By subcategory =By.xpath("//a[@title=\"Show products in category Shoes\"]");
+        WebElement SubCategory = browser.driver.findElement(subcategory);
+        return SubCategory;
+
+
+    }
+
 }
--- E-Commerce app/src/test/java/stepDefinitions/SC6_select_Dif_categoriesSD.java
+++ E-Commerce app/src/test/java/stepDefinitions/SC6_select_Dif_categoriesSD.java
@@ -1,15 +1,37 @@
 package stepDefinitions;
 
+import Pages.SC6_select_Dif_categoriesPage;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
 public class SC6_select_Dif_categoriesSD {
 
+    SC6_select_Dif_categoriesPage Cat = new SC6_select_Dif_categoriesPage();
 
 
+    @When("user click on category")
+    public void user_click_on_category() {
+        Cat.Category().click();
+    }
+
+
+    @And("user click on sub-category")
+    public void user_click_on_sub_category() throws InterruptedException {
+        Cat.SubCategory().click();
+        Thread.sleep(3000);
+    }
+
+
+    @Then("category page will open")
+    public void category_page_will_open() {
+        String expected = browser.driver.findElement(By.className("page-title")).getText();
+        String actual = "Shoes";
+        Assert.assertTrue(expected.contains(actual),"done");
 
     }
+}
 
 
